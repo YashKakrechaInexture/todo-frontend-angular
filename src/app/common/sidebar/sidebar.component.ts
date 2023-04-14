175639a5a863ae14a02431ec37d40d4726679401
YashKakrechaInexture <yashvk.inexture@gmail.com>
2023-04-14 17:55:58 +0530

    Removed comments

--- src/app/common/sidebar/sidebar.component.ts
+++ src/app/common/sidebar/sidebar.component.ts
@@ -17,13 +17,6 @@ export class SidebarComponent implements OnInit{
   }
   ngOnInit(): void {
     this.clickEvent.emit(this.isClicked);
-      // const ulElement = this.el.nativeElement.querySelector(`.item-show-${id}`) as HTMLElement;
-      // const isActive = ulElement.querySelector('.active') !== null;
-      // if(isActive){
-      //   this.renderer.setStyle(ulElement, 'display', 'block');
-      // } else {
-      //   this.renderer.setStyle(ulElement, 'display', displayValue);
-      // }
   }
 
   toggleClick(event:any) {
@@ -31,17 +24,12 @@ export class SidebarComponent implements OnInit{
     this.clickEvent.emit(this.isClicked);
     if (this.isClicked) {
       // Move the main element to the right when the sidebar is opened
-      // this.renderer.setStyle(document.body, 'position', 'relative');
-      // this.renderer.setStyle(document.body, 'left', '250px');
-      // this.renderer.setStyle(document.body, 'transition', 'left ease-in-out');
       this.renderer.setStyle(document.body, 'margin-left', '250px');
       this.renderer.setStyle(document.body, 'transition-timing-function', 'ease');
       this.renderer.setStyle(document.body, 'transition-property', 'all');
       this.renderer.setStyle(document.body, 'transition-duration', '0.4s');
     } else {
       // Reset the position of the main element when the sidebar is closed
-      // this.renderer.removeStyle(document.body, 'position');
-      // this.renderer.removeStyle(document.body, 'left');
       this.renderer.setStyle(document.body, 'margin-left', '0px');
       this.renderer.setStyle(document.body, 'transition-timing-function', 'ease');
       this.renderer.setStyle(document.body, 'transition-property', 'all');
@@ -54,24 +42,6 @@ export class SidebarComponent implements OnInit{
     const ulElement = this.el.nativeElement.querySelector(`.item-show-${id}`) as HTMLElement;
     const spanElement = event.target.querySelector('span') as HTMLElement;
     spanElement?.classList?.toggle('rotate');
-
-    // const displayValue = ulElement.style.display === 'none' || ulElement.style.display === '' ? 'block' : 'none';
-    // this.renderer.setStyle(ulElement, 'display', displayValue);
-    
-    // const displayValue = ulElement.style.display === 'none' || ulElement.style.display === '' ? true : false;
     ulElement?.classList?.toggle('close-dropdown');
-    // console.log(this.isClicked);
-    // if(this.isClicked){
-    //   this.renderer.addClass(ulElement, 'open-dropdown');
-    // }else{
-    //   this.renderer.removeClass(ulElement, 'open-dropdown');
-    // }
-    
-    // const isActive = ulElement.querySelector('.active') !== null;
-    // if(isActive){
-    //   this.renderer.setStyle(ulElement, 'display', 'block');
-    // } else {
-    //   this.renderer.setStyle(ulElement, 'display', displayValue);
-    // }
   }
 }
